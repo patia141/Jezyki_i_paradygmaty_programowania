4c5563cc6a16b91e4ce67cfbb9e88c40b6863dda
maxwell the cat <115742765+patia141@users.noreply.github.com>
2025-01-07 00:06:26 +0100

    Add files via upload

---
+++ lab7/zad1.fs
@@ -0,0 +1,84 @@
+﻿open System
+open System.Collections.Generic
+
+type Status =
+    | Dostepna
+    | Wypozyczona
+
+type Book(title: string, author: string, pages: int) =
+    let mutable status = "dostępna"
+    member this.Title = title
+    member this.Author = author
+    member this.Pages = pages
+    member this.Status
+        with get() = status
+        and set(value) = status <- value
+
+    member this.GetInfo() =
+        sprintf "Tytuł: %s \tAutor: %s \tLiczba stron: %d \tStatus: %s" this.Title this.Author this.Pages this.Status
+
+
+type User(name: string) =
+    member this.Name = name
+    member this.BorrowedBooks = new List<Book>()
+
+    member this.BorrowBook(book: Book) =
+        if book.Status = "dostępna" then
+            this.BorrowedBooks.Add(book)
+            book.Status <- "wypożyczona"
+            printfn "%s wypożyczył/a książkę %s" this.Name book.Title
+        else
+            printfn "Książka %s jest już wypożyczona i niedostępna" book.Title
+
+    member this.ReturnBook(book: Book) =
+        book.Status <- "dostępna"
+        this.BorrowedBooks.Remove(book) |> ignore
+        printfn "%s zwrócił/a książkę %s" this.Name book.Title
+
+
+type Library() = 
+    let books = new List<Book>()
+
+    member this.AddBook(book: Book) =
+        books.Add(book)
+        printfn "Dodano książkę %s" book.Title
+
+    member this.RemoveBook(book: Book) =
+        if books.Remove(book) then
+            printfn "Usunięto książkę %s" book.Title
+        else
+            printfn "Nie znaleziono książki %s" book.Title
+
+    member this.ListBooks() =
+        printfn "\nKsiążki w bibliotece:"
+        for book in books do
+            printfn "- %s" (book.GetInfo())
+        printfn "--------------------------------------------------------------------------------------"
+
+
+[<EntryPoint>]
+let main argv =
+    let library = Library()
+
+    let book1 = Book("Książka 1", "Autor 1", 123)
+    let book2 = Book("Książka 2", "Autor 2", 321)
+    let book3 = Book("Książka 3", "Autor 3", 420)
+
+    library.AddBook(book1)
+    library.AddBook(book2)
+    library.AddBook(book3)
+    library.ListBooks()
+
+    let user = User("Jan Kowalski")
+
+    user.BorrowBook(book1)
+    user.BorrowBook(book2)
+    user.BorrowBook(book3)
+    library.ListBooks()
+
+    user.ReturnBook(book1)
+    user.ReturnBook(book3)
+    user.BorrowBook(book2)
+    library.ListBooks()
+
+    0
---
+++ lab7/zad2.fs
@@ -0,0 +1,76 @@
+﻿open System
+
+type BankAccount(accountNumber: string, initialBalance: decimal) =
+    member val AccountNumber = accountNumber with get
+    member val Balance = initialBalance with get, set
+
+    member this.Deposit(amount: decimal) =
+        if amount <= 0m then
+            printfn "Kwota wpłaty musi być większa niż 0"
+        else
+            this.Balance <- this.Balance + amount
+            printfn "Wpłacono %A. Aktualne saldo: %A" amount this.Balance
+
+    member this.Withdraw(amount: decimal) =
+        if amount <= 0m then
+            printfn "Kwota wypłaty musi być większa niż 0"
+        elif amount > this.Balance then
+            printfn "Niewystarczające środki na koncie. Aktualne saldo: %A" this.Balance
+        else
+            this.Balance <- this.Balance - amount
+            printfn "Wypłacono %A. Aktualne saldo: %A" amount this.Balance
+
+
+type Bank() =
+    let accounts = new System.Collections.Generic.Dictionary<string, BankAccount>()
+
+    member this.CreateAccount(accountNumber: string, initialBalance: decimal) =
+        if accounts.ContainsKey(accountNumber) then
+            printfn "Konto %s już istnieje" accountNumber
+        else
+            let account = new BankAccount(accountNumber, initialBalance)
+            accounts.Add(accountNumber, account)
+            printfn "Konto %s zostało utworzone. Saldo: %A" accountNumber initialBalance
+
+    member this.GetAccount(accountNumber: string) =
+        if accounts.ContainsKey(accountNumber) then
+            Some(accounts.[accountNumber])
+        else
+            printfn "Konto %s nie istnieje" accountNumber
+            None
+
+    member this.UpdateAccount(accountNumber: string, newBalance: decimal) =
+        match this.GetAccount(accountNumber) with
+        | None -> ()
+        | Some(account) ->
+            account.Balance <- newBalance
+            printfn "Saldo konta %s zostało zaktualizowane. Nowe saldo: %A" accountNumber newBalance
+
+    member this.DeleteAccount(accountNumber: string) =
+        if accounts.Remove(accountNumber) then
+            printfn "Konto %s zostało usunięte." accountNumber
+        else
+            printfn "Konto %s nie istnieje." accountNumber
+            
+
+[<EntryPoint>]
+let main argv =
+    let bank = Bank()
+    bank.CreateAccount("123", 1000.0m)
+    bank.CreateAccount("654", 500.0m)
+
+    bank.GetAccount("123") 
+    |> Option.iter (fun account ->
+        account.Deposit(200.0m)
+        account.Withdraw(150.0m))
+
+    bank.GetAccount("654")
+    |> Option.iter (fun account ->
+        account.Deposit(50.0m)
+        account.Withdraw(1000.0m))
+
+    bank.UpdateAccount("123", 2000.0m)
+    bank.DeleteAccount("654")
+    bank.GetAccount("654") |> ignore
+
+    0
